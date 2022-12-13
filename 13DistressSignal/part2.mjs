a67f153d74982d666f0be7030d9f5d9595da4cd8
Andrew Graham <ag@andrewgraham.co.uk>
2022-12-13 08:39:29 +0000

    Day 13 solution

--- 13DistressSignal/part2.mjs
+++ 13DistressSignal/part2.mjs
@@ -7,55 +7,54 @@ var file = readline.createInterface({
 
 const signals = []
 
-let currentPair = []
-let sumOfIndices = 0
-
 file.on('line', (line) => {
-  if (line === '') {
-    signals.push(currentPair)
-    currentPair = []
-    return
+  if (line !== '') {
+    signals.push(JSON.parse(line))
   }
-  currentPair.push(JSON.parse(line))
 })
 
 const compare = function(l, r) {
   // if left side or right side ran out of items
   if (typeof l === 'undefined') {
-    return true
+    return -1
   }
   if (typeof r === 'undefined') {
-    return false
+    return 1
   }
   // both integers
   if (!Array.isArray(l) && !Array.isArray(r)) {
     if (l < r) {
-      return true
+      return -1
     }
     if (l > r) {
-      return false
+      return 1
     }
-    return null
+    return 0
   }
-  // otherwise convert to arrays
+  // convert to arrays
   const lArray = (Array.isArray(l) ? [...l] : [l])
   const rArray = (Array.isArray(r) ? [...r] : [r])
-
+  // both arrays
   while (lArray.length > 0 || rArray.length > 0) {
     const result = compare(lArray.shift(), rArray.shift()) 
-    if (result !== null) {
+    if (result !== 0) {
       return result
     }    
   }
-  return null
+  return 0
 }
 
 file.on('close', () => {
-  signals.push(currentPair)
-  signals.forEach(([leftPair, rightPair], index) => {
-    if (compare(leftPair, rightPair)) {
-      sumOfIndices += index + 1
+  signals.push([[2]])
+  signals.push([[6]])
+  signals.sort(compare)
+  let key = 1
+  signals.forEach((signal, index) => {
+    const signalString = JSON.stringify(signal)
+    if (signalString === '[[2]]' || signalString === '[[6]]') {
+      key *= index + 1
     }
   })
-  console.log('Part 1: pairs in order =', sumOfIndices)
+
+  console.log('Part 2: decoding key =', key)
 })
