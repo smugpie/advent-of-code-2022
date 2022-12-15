265cc7424b675278be47c5e2d9dbc25bdb093e45
Andrew Graham <andrew.graham@bbc.co.uk>
2022-12-15 08:10:34 +0000

    Typescript day 1

--- typescript/01CalorieCounting/parts1and2.js
+++ typescript/01CalorieCounting/parts1and2.js
@@ -1,9 +1,9 @@
 "use strict";
 exports.__esModule = true;
-var fs_1 = require("fs");
-var readline_1 = require("readline");
-var file = readline_1["default"].createInterface({
-    input: fs_1["default"].createReadStream('./input.txt')
+var fs = require("fs");
+var readline = require("readline");
+var file = readline.createInterface({
+    input: fs.createReadStream('./input.txt')
 });
 var elfCalories = [];
 var currentElfCalories = 0;
@@ -13,12 +13,12 @@ file.on('line', function (calories) {
         currentElfCalories = 0;
     }
     else {
-        currentElfCalories += calories;
+        currentElfCalories += +calories;
     }
 });
 file.on('close', function () {
     elfCalories.push(currentElfCalories);
-    var _a = elfCalories.sort(function (a, b) { return b - a; }), first = _a[0], number = _a[1], second = _a[2], number = _a[3], third = _a[4], number = _a[5];
+    var _a = elfCalories.sort(function (a, b) { return b - a; }), first = _a[0], second = _a[1], third = _a[2];
     console.log('Part 1: Most calories =', first);
     console.log('Part 2: Sum of top three calories =', first + second + third);
 });
--- typescript/01CalorieCounting/parts1and2.ts
+++ typescript/01CalorieCounting/parts1and2.ts
@@ -1,11 +1,11 @@
-import fs from 'fs'
-import readline from 'readline'
+import * as fs from 'fs'
+import * as readline from 'readline'
 
 var file = readline.createInterface({
   input: fs.createReadStream('./input.txt')
 })
 
-const elfCalories: [number?] = []
+const elfCalories: number[] = []
 let currentElfCalories: number = 0
 
 file.on('line', (calories: string) => {
@@ -13,13 +13,13 @@ file.on('line', (calories: string) => {
     elfCalories.push(+currentElfCalories)
     currentElfCalories = 0
   } else {
-    currentElfCalories += calories
+    currentElfCalories += +calories
   }
 })
 
 file.on('close', () => {
   elfCalories.push(currentElfCalories)
-  const [first: number, second: number, third: number] = elfCalories.sort((a: number, b: number): number => b - a)
+  const [first, second, third] = elfCalories.sort((a: number, b: number): number => b - a)
   console.log('Part 1: Most calories =', first)
   console.log('Part 2: Sum of top three calories =', first + second + third)
 })
