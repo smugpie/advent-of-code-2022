5c2d476e110c849be13dec3f5b81f05ea28485bf
Andrew Graham <andrew.graham@bbc.co.uk>
2022-12-15 14:57:20 +0000

    Day 15 part 1 complete

--- node/15BeaconExclusion/part1.mjs
+++ node/15BeaconExclusion/part1.mjs
@@ -2,38 +2,38 @@ import fs from 'fs'
 import readline from 'readline'
 
 var file = readline.createInterface({
-  input: fs.createReadStream('./testInput.txt')
+  input: fs.createReadStream('./input.txt')
 })
 
-let rowToCheck = 10
+let rowToCheck = 2000000
 const positions = new Set()
+const beaconsAtRow = []
 
 file.on('line', (beacon) => {
-  console.log(beacon)
   const [, sensorX, sensorY, beaconX, beaconY] = beacon.match(
     /Sensor at x=([0-9-]+), y=([0-9-]+): closest beacon is at x=([0-9-]+), y=([0-9-]+)/i
   )
   const verticalDistance = Math.abs(+sensorY - +beaconY)
   const horizontalDistance = Math.abs(+sensorX - +beaconX)
   const distance = horizontalDistance + verticalDistance
-  console.log(sensorX, sensorY, beaconX, beaconY, distance)
 
+  // if our row is close enough to the sensor then start plotting
   const distanceFromCheck = Math.abs(+sensorY - rowToCheck)
-  console.log(distanceFromCheck, 'rows away')
-  if (distanceFromCheck > distance) {
-    console.log('too far away to make a difference')
-  } else {
-    console.log('adding ', sensorX)
+  if (distanceFromCheck <= distance) {
     positions.add(+sensorX)
     for (let i = 1; i <= Math.abs(distance - distanceFromCheck); i += 1) {
-      console.log('adding ', +sensorX + i)
-      console.log('adding ', +sensorX - i)
       positions.add(+sensorX + i)
       positions.add(+sensorX - i)
     }
   }
+  if (+beaconY === rowToCheck) {
+    beaconsAtRow.push(+beaconX)
+  }
 })
 
 file.on('close', () => {
-  console.log('Part 1: number of invalidated positions =', positions.size, Array.from(positions).sort((a, b) => +b - +a))
+
+  // remove location of beacons we know about  
+  beaconsAtRow.forEach(location => positions.delete(location))
+  console.log('Part 1: number of non-beacon positions =', positions.size)
 })
